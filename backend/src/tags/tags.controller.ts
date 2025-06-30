39daa80e0ac6e6b78791aa1c44edb070ad543904
nrivas03 <n.rivas03@ufromail.cl>
2025-06-30 04:52:02 -0400

    refactor: remove create tag functionality from TagsService and TagsController

--- backend/src/tags/tags.controller.ts
+++ backend/src/tags/tags.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 import { TagsService } from './tags.service';
 import { Tag } from './entities/tag.entity';
 
@@ -9,13 +9,5 @@ export class TagsController {
   @Get()
   findAll(): Promise<Tag[]> {
     return this.tagsService.findAll();
-  }
-
-  @Post()
-  create(@Body('name') name: string): Promise<Tag> {
-    if (!name) {
-      throw new Error('Tag name is required');
-    }
-    return this.tagsService.create(name);
   }
 }
--- backend/src/tags/tags.service.ts
+++ backend/src/tags/tags.service.ts
@@ -15,23 +15,6 @@ export class TagsService {
   }
 
   /**
-   * Creates a new tag with the given name.
-   * If a tag with the same name already exists, an error is thrown.
-   * @param name - The name of the tag to create.
-   * @returns Promise<Tag> - The created Tag entity.
-   * @throws Error if a tag with the same name already exists.
-   */
-  async create(name: string): Promise<Tag> {
-    const [tag, created] = await this.tagModel.findOrCreate({
-      where: { name },
-    });
-    if (!created) {
-      throw new Error(`Tag with name "${name}" already exists`);
-    }
-    return tag;
-  }
-
-  /**
    * Finds or creates multiple tags based on the provided names.
    * @param names - An array of tag names to find or create.
    * @returns Promise<Tag[]> - An array of Tag entities.
